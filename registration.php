e7ef0e543eae82db6247f426f3bcfeb28bdef114
mjuc <michal.jucewicz@protonmail.com>
2022-01-27 17:24:55 +0100

    Added password validation.

--- registration.php
+++ registration.php
@@ -21,16 +21,54 @@
             $telefon_komorkowy = $_GET['telefon_komorkowy_'];
             $haslo1 = $_GET['haslo1_'];
             $haslo2 = $_GET['haslo2_'];
+
+            function validatePaswd($h1)
+            {
+                if( strlen($h1 ) < 8 ) {
+                    $error .= "Password too short!
+                    ";
+                    }
+                    if( strlen($h1 ) > 20 ) {
+                    $error .= "Password too long!
+                    ";
+                    }
+                    if( strlen($h1 ) < 8 ) {
+                    $error .= "Password too short!
+                    ";
+                    }
+                    if( !preg_match("#[0-9]+#", $h1 ) ) {
+                    $error .= "Password must include at least one number!
+                    ";
+                    }
+                    if( !preg_match("#[a-z]+#", $h1 ) ) {
+                    $error .= "Password must include at least one letter!
+                    ";
+                    }
+                    if( !preg_match("#[A-Z]+#", $h1 ) ) {
+                    $error .= "Password must include at least one CAPS!
+                    ";
+                    }
+                    if( !preg_match("#W+#", $h1 ) ) {
+                    $error .= "Password must include at least one symbol!
+                    ";
+                    }
+                    if($error){
+                    echo "Password validation failure(your choise is weak): $error";
+                    return(false);
+                    } else {
+                    return(true);
+                    }
+            }
         
             if ($haslo1 == $haslo2)
             {
                 $dsn = 'mysql:dbname=clock;host=46.41.140.79;port=3306;charset=utf8';
                 $username = 'clockadmin';
-                $password = 'VDm9T-Y#8b_Q4qqj';
+                $h1 = 'VDm9T-Y#8b_Q4qqj';
 
                 try 
                 {
-                    $connection = new \PDO($dsn, $username, $password);
+                    $connection = new \PDO($dsn, $username, $h1);
                     $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                     $connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
                     echo "Połączono prawidłowo\n";
@@ -38,6 +76,10 @@
                 catch(PDOException $e)
                 {
                     echo "Nie połączono, następujący błąd: " . $e->getMessage();
+                }
+                if (! validatePaswd($haslo1))
+                {
+                    echo("Hasło nie spełnia warunków.");
                 }
 
                 if ($connection == true)
