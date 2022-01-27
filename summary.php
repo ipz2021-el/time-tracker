679a18f3409e51394428ac844dbe5a5ea643c6a8
Tescik Tescikowski <tescik.tescikowski@gmail.com>
2022-01-27 04:32:22 +0100

    sql query

--- summary.php
+++ summary.php
@@ -34,72 +34,70 @@ class Summary {
     $result -> free_result();
     $mysqli -> close();
   }
+
   // week
   function get_week() {
     return $this->week;
   }
   function set_week() {
-    // $mysqli = new mysqli(DBhost, DBuser, DBpass, DBname, DBport);
-    // if ($mysqli -> connect_errno) {
-    //   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    //   exit();
-    // }
-    // $query = "SELECT czas_start, czas_stop FROM czas_pracy;";
-    // $result = $mysqli->query($query);
-    // if (!$result) {
-    //   throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
-    // }
-    // if ($row = $result->fetch_assoc()) {
-    //   $this->people = $row['users'];
-    // }
-    // $result -> free_result();
-    // $mysqli -> close();
-    $this->week = 0;
+    $mysqli = new mysqli(DBhost, DBuser, DBpass, DBname, DBport);
+    if ($mysqli -> connect_errno) {
+      echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+      exit();
+    }
+    $query = "select czas_start, czas_stop from czas_pracy WHERE czas_start between date_sub(now(),INTERVAL 1 WEEK) and now();";
+    $result = $mysqli->query($query);
+    $sum = 0;
+    while ($row = $result->fetch_assoc()) {
+      $sum += floor((strtotime($row["czas_stop"]) - strtotime($row["czas_start"])) / 3600) ;
+    }
+    $result -> free_result();
+    $mysqli -> close();
+    $this->week = $sum;
   }
+
   // month
   function get_month() {
     return $this->month;
   }
   function set_month() {
-    // $mysqli = new mysqli(DBhost, DBuser, DBpass, DBname, DBport);
-    // if ($mysqli -> connect_errno) {
-    //   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    //   exit();
-    // }
-    // $query = "SELECT czas_start, czas_stop FROM czas_pracy;";
-    // $result = $mysqli->query($query);
-    // if (!$result) {
-    //   throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
-    // }
-    // if ($row = $result->fetch_assoc()) {
-    //   $this->people = $row['users'];
-    // }
-    // $result -> free_result();
-    // $mysqli -> close();
-    $this->month = 0;
+    $mysqli = new mysqli(DBhost, DBuser, DBpass, DBname, DBport);
+    if ($mysqli -> connect_errno) {
+      echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+      exit();
+    }
+    $query = "select czas_start, czas_stop from czas_pracy WHERE czas_start between date_sub(now(),INTERVAL 1 MONTH) and now();";
+    $result = $mysqli->query($query);
+    $sum = 0;
+    while ($row = $result->fetch_assoc()) {
+      $sum += floor((strtotime($row["czas_stop"]) - strtotime($row["czas_start"])) / 3600) ;
+    }
+    $result -> free_result();
+    $mysqli -> close();
+    $this->month = $sum;
   }
+
   // year
   function get_year() {
     return $this->year;
   }
   function set_year() {
-    // $mysqli = new mysqli(DBhost, DBuser, DBpass, DBname, DBport);
-    // if ($mysqli -> connect_errno) {
-    //   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    //   exit();
-    // }
-    // $query = "SELECT czas_start, czas_stop FROM czas_pracy;";
-    // $result = $mysqli->query($query);
-    // if (!$result) {
-    //   throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
-    // }
-    // if ($row = $result->fetch_assoc()) {
-    //   $this->people = $row['users'];
-    // }
-    // $result -> free_result();
-    // $mysqli -> close();
-    $this->year = 0;
+    $mysqli = new mysqli(DBhost, DBuser, DBpass, DBname, DBport);
+    if ($mysqli -> connect_errno) {
+      echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+      exit();
+    }
+    $query = "select czas_start, czas_stop from czas_pracy WHERE czas_start between date_sub(now(),INTERVAL 1 YEAR) and now();";
+    $result = $mysqli->query($query);
+    $sum = 0;
+    while ($row = $result->fetch_assoc()) {
+      $sum += floor((strtotime($row["czas_stop"]) - strtotime($row["czas_start"])) / 3600) ;
+    }
+    $result -> free_result();
+    $mysqli -> close();
+    $this->year = $sum;
   }
+
   // all
   function get_all() {
     return $this->all;
@@ -112,16 +110,15 @@ class Summary {
     }
     $query = "SELECT czas_start, czas_stop FROM czas_pracy;";
     $result = $mysqli->query($query);
+    $sum = 0;
     while ($row = $result->fetch_assoc()) {
-      echo 'start' . strtotime($row["czas_stop"]);
-      echo 'stop' . strtotime($row["czas_start"]) . '\n';
-      echo floor((strtotime($row["czas_stop"]) - strtotime($row["czas_start"])) / 3600) ;
+      // echo 'start' . strtotime($row["czas_stop"]);
+      // echo 'stop' . strtotime($row["czas_start"]) . '\n';
+      $sum += floor((strtotime($row["czas_stop"]) - strtotime($row["czas_start"])) / 3600) ;
     }
-    
-    
     $result -> free_result();
     $mysqli -> close();
-    $this->all = 0;
+    $this->all = $sum;
   }
 
   function set_vars() {
