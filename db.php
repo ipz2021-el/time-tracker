4a8b7e5d5273795cab04ee89442259f5fada622a
Tescik Tescikowski <tescik.tescikowski@gmail.com>
2022-01-27 03:43:38 +0100

    sql query

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-    define('DBname', 'database_name_here');
-    define('DBuser', 'username_here');
-    define('DBpass', 'password_here');
+    define('DBname', 'clock');
+    define('DBuser', 'clockadmin');
+    define('DBpass', 'VDm9T-Y#8b_Q4qqj');
     define('DBhost', 'localhost');
     define('DBport', 3306);
 ?>
