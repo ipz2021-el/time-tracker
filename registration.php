b59d3bfcf918410b7a0b8931cab8f5b83cc63b36
Pawel-Gaborek <44684528+Pawel-Gaborek@users.noreply.github.com>
2022-01-27 15:00:10 +0100

    Aktualizacja kodu

--- registration.php
+++ registration.php
@@ -1,89 +1,83 @@
-<!DOCTYPE html>
-<html lang="en">
+
+<html>
     <head>
-        <meta charset="UTF-8">
-        <title>FORMULARZ REJESTRACJI</title> </head>
-        <link rel="stylesheet" href="lproject.css">
-	    <script src="lproject.js"></script>
-<body>
-    
-        <div>
-            
-            FORMULARZ REJESTRACJI
-            
-        </div>
-        <!-- tego nie wiem czy to jest ok-->
-        <form action="post_registration.php" onsubmit="return validate_registration()" method="post" enctype="text/plain"><div>
-            
-            <!-- Podstawowe pole tekstowe -->
-            <div class="oneinput">
-            <label for="imie">Podaj imię: </label><br>
-            <input type="text" id="imie" name="imie" placeholder="Jan">
-            </div>
-            
-            <div class="oneinput">
-            <label for="nazwisko">Podaj nazwisko: </label><br>
-            <input type="text" id="nazwisko" name="nazwisko" placeholder="Kowalski">
-            </div>
-                
-            <div class="oneinput">
-            <label for="adres_ulica">Podaj nazwę ulicy: </label><br>
-            <input type="text" id="adres_ulica" name="adres_ulica" placeholder="Aleje jerozolimskie">
-            </div>
-            
-            <div class="oneinput">
-            <label for="adres_numer_domu_mieszkania">Podaj numer budynku: </label><br>
-            <input type="text" id="adres_numer_domu_mieszkania" name="adres_numer_domu_mieszkania" placeholder="1000">
-            </div>
-            
-            <div class="oneinput">
-            <label for="adres_miasto">Podaj nazwę miasta: </label><br>
-            <input type="text" id="adres_miasto" name="adres_miasto" placeholder="Warszawa">
-            </div>
-            
-            <div class="oneinput">
-            <label for="adres_kod_pocztowy">Podaj kod pocztowy: </label><br>
-            <input type="text" id="adres_kod_pocztowy" name="adres_kod_pocztowyy" placeholder="11-111" pattern="[0-9]{2}-[0-9]{3}"><br>
-            <small>Format: 11-111</small>
-            </div>
-            
-            <div class="oneinput">
-            <label for="adres_kraj">Podaj nazwę kraju: </label><br>
-            <input type="text" id="adres_kraj" name="adres_kraj" placeholder="Polska">
-            </div>
-            
-            
-            <div class="oneinput">
-            <label for="email">Podaj email: </label><br>
-            <input type="text" id="email" name="email" placeholder="jan@kowalski.pl">
-            </div>
-            
-            <div class="oneinput">
-            <label for="haslo">Podaj haslo: </label><br>
-            <input type="text" id="haslo" name="haslo" placeholder="haslo123!">
-            </div>
-            
-            <div class="oneinput">
-            <label for="haslo">Podaj haslo: </label><br>
-            <input type="text" id="haslo" name="haslo" placeholder="haslo123!">
-            </div>
-            
-            <div class="oneinput">
-            <label for="telefon_komorkowy">Podaj telefon komórkowy: </label><br>
-            <input type="tel" id="telefon_komorkowy" name="telefon_komorkowy" placeholder="111-111-111" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}"><br>
-            <small>Format: 111-111-111</small>
-            </div>
-            
-        <!-- Przycisk WYŚLIJ -->
-        <input type="submit" value="Rejestruj użytkownika">
-        <!-- Przycisk WYCZYŚĆ DANE -->
-        <input type="reset" value="Wyczyść dane">
-        </div></form>
-    
-    
+        <title>
+            SKRYPT WYSYLKI
+        </title>
+    </head>
 
-</body>
+    <body>
+        
+        <?php
+            $imie = $_GET['imie_'];
+            $nazwisko = $_GET['nazwisko_'];
+            $adres_ulica = $_GET['adres_ulica_'];
+            $adres_numer_domu_mieszkania = $_GET['adres_numer_domu_mieszkania_'];
+            $adres_miasto = $_GET['adres_miasto_'];
+            $adres_kod_pocztowy = $_GET['adres_kod_pocztowy_'];
+            $adres_kraj = $_GET['adres_kraj_'];
+            $klasa_uzytkownika = $_GET['klasa_uzytkownika_'];
+            $email = $_GET['email_'];
+            $telefon_komorkowy = $_GET['telefon_komorkowy_'];
+            $haslo1 = $_GET['haslo1_'];
+            $haslo2 = $_GET['haslo2_'];
+        
+            if ($haslo1 == $haslo2)
+            {
+                $dsn = 'mysql:dbname=clock;host=46.41.140.79;port=3306;charset=utf8';
+                $username = 'clockadmin';
+                $password = 'VDm9T-Y#8b_Q4qqj';
+
+                try 
+                {
+                    $connection = new \PDO($dsn, $username, $password);
+                    $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                    $connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+                    echo "Połączono prawidłowo\n";
+                }
+                catch(PDOException $e)
+                {
+                    echo "Nie połączono, następujący błąd: " . $e->getMessage();
+                }
+
+                if ($connection == true)
+                {   
+                    $conn = mysqli_connect("46.41.140.79", "clockadmin", "VDm9T-Y#8b_Q4qqj", "clock");
+
+                    $sql = "INSERT INTO uzytkownik (imie, nazwisko, adres_ulica, adres_numer_domu_mieszkania, adres_miasto, adres_kod_pocztowy, adres_kraj, klasa_uzytkownika, email, telefon_komorkowy, haslo)
+                            VALUES ('$imie', '$nazwisko', '$adres_ulica', '$adres_numer_domu_mieszkania', '$adres_miasto', '$adres_kod_pocztowy', '$adres_kraj', '$klasa_uzytkownika', '$email', '$telefon_komorkowy', '$haslo1')";
+                            
+
+                    if($conn == false)
+                    {
+                        die("Błąd połączenia". mysqli_connect_error());
+                    }
+                    
+                    if(mysqli_query($conn, $sql))
+                    {
+                        echo "Dodano rekord";
+                        include 'login.html';
+                    }
+
+                    else
+                    {
+                    echo "Nie dodano rekordu $sql. " 
+                        . mysqli_error($conn);
+                    }
+                }
+                else
+                {
+                    echo "Nie dodano rekordu\n";
+                }
+            }
+            else
+            {
+                echo "Hasla sa rózne";
+                echo "Podaj ponownie haslo";
+                include 'registration.html';
+            }
+        ?>
+    <br>
+
+    </body>
 </html>
-
-
-
