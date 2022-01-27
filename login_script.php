aa882ba6b7b79dacbe56ce8169bdf1811461262e
Pawel-Gaborek <it@pawel.gaborek.com>
2022-01-27 17:40:25 +0100

    Aktualizacja
    Aktualizacja gaborek

---
+++ login_script.php
@@ -0,0 +1,77 @@
+
+<html>
+    <head>
+        <title>
+            SKRYPT WYSYLKI
+        </title>
+    </head>
+
+    <body>
+        <?php
+                $poprawne_dane = false;
+                $email = $_GET['email_'];
+                $haslo = $_GET['haslo_'];
+
+                $dsn = 'mysql:dbname=clock;host=46.41.140.79;port=3306;charset=utf8';
+                $username = 'clockadmin';
+                $password = 'VDm9T-Y#8b_Q4qqj';
+                try 
+                {
+                    $connection = new \PDO($dsn, $username, $password);
+                    $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                    $connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+                    echo "Połączono prawidłowo\n";
+                }
+                catch(PDOException $e)
+                {
+                    echo "Nie połączono, następujący błąd: " . $e->getMessage();
+                }
+
+                if ($connection == true)
+                {   
+                    $conn = mysqli_connect("46.41.140.79", "clockadmin", "VDm9T-Y#8b_Q4qqj", "clock");
+                    $sql = "SELECT email, haslo FROM uzytkownik";
+
+                    if($conn == false)
+                    {
+                        die("Błąd połączenia". mysqli_connect_error());
+                    }
+                    else
+                    {
+                        $wynik = mysqli_query($conn,$sql);
+                        if($wynik == false)
+                        {
+                            echo "Bląd zapytania SQL";
+                            include 'login.php';
+                        }
+                        else
+                        {
+                            while($row = mysqli_fetch_array($wynik))
+                            {
+                                $temp_email = $row['email'];
+                                $temo_haslo = $row['haslo']; 
+                                if (($temp_email == $email) and ($temo_haslo  == $haslo))
+                                {
+                                    $poprawne_dane = true;
+                                }
+                            }
+                        }
+                    }
+                    if ($poprawne_dane == true)
+                    {
+                        if(file_exists("privat.php")) 
+                        {
+                            include 'privat.php';
+                        }
+                        echo "Zalogowano poprawnie";
+                    }
+                    mysqli_close($conn);
+                }
+                else
+                {
+                    echo "Błąd połączenia";
+                }
+        ?>
+    <br>
+    </body>
+</html>
---
+++ registration_script.php
@@ -0,0 +1,83 @@
+
+<html>
+    <head>
+        <title>
+            SKRYPT WYSYLKI
+        </title>
+    </head>
+
+    <body>
+        
+        <?php
+            $imie = $_GET['imie_'];
+            $nazwisko = $_GET['nazwisko_'];
+            $adres_ulica = $_GET['adres_ulica_'];
+            $adres_numer_domu_mieszkania = $_GET['adres_numer_domu_mieszkania_'];
+            $adres_miasto = $_GET['adres_miasto_'];
+            $adres_kod_pocztowy = $_GET['adres_kod_pocztowy_'];
+            $adres_kraj = $_GET['adres_kraj_'];
+            $klasa_uzytkownika = $_GET['klasa_uzytkownika_'];
+            $email = $_GET['email_'];
+            $telefon_komorkowy = $_GET['telefon_komorkowy_'];
+            $haslo1 = $_GET['haslo1_'];
+            $haslo2 = $_GET['haslo2_'];
+        
+            if ($haslo1 == $haslo2)
+            {
+                $dsn = 'mysql:dbname=clock;host=46.41.140.79;port=3306;charset=utf8';
+                $username = 'clockadmin';
+                $password = 'VDm9T-Y#8b_Q4qqj';
+
+                try 
+                {
+                    $connection = new \PDO($dsn, $username, $password);
+                    $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                    $connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+                    echo "Połączono prawidłowo\n";
+                }
+                catch(PDOException $e)
+                {
+                    echo "Nie połączono, następujący błąd: " . $e->getMessage();
+                }
+
+                if ($connection == true)
+                {   
+                    $conn = mysqli_connect("46.41.140.79", "clockadmin", "VDm9T-Y#8b_Q4qqj", "clock");
+
+                    $sql = "INSERT INTO uzytkownik (imie, nazwisko, adres_ulica, adres_numer_domu_mieszkania, adres_miasto, adres_kod_pocztowy, adres_kraj, klasa_uzytkownika, email, telefon_komorkowy, haslo)
+                            VALUES ('$imie', '$nazwisko', '$adres_ulica', '$adres_numer_domu_mieszkania', '$adres_miasto', '$adres_kod_pocztowy', '$adres_kraj', '$klasa_uzytkownika', '$email', '$telefon_komorkowy', '$haslo1')";
+                            
+
+                    if($conn == false)
+                    {
+                        die("Błąd połączenia". mysqli_connect_error());
+                    }
+                    
+                    if(mysqli_query($conn, $sql))
+                    {
+                        echo "Dodano rekord";
+                        include 'login.php';
+                    }
+
+                    else
+                    {
+                    echo "Nie dodano rekordu $sql. " 
+                        . mysqli_error($conn);
+                    }
+                }
+                else
+                {
+                    echo "Nie dodano rekordu\n";
+                }
+            }
+            else
+            {
+                echo "Hasla sa rózne";
+                echo "Podaj ponownie haslo";
+                include 'registration.php';
+            }
+        ?>
+    <br>
+
+    </body>
+</html>
