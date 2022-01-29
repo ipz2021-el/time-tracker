e6533977d4a341e97e7f11e95ff6723bf07b5a40
Pawel-Gaborek <it@pawel.gaborek.com>
2022-01-29 20:31:18 +0100

    Update email_script.php

--- email_script.php
+++ email_script.php
@@ -1,2 +1,34 @@
+<?php
+	class send_email 
+	{
+		function send_email($email, $haslo) 
+		{
+			$msg="";
+			if($email)
+			{
+
+				$from_add = "aplikacjeinternetowe2022@gmail.com"; 
+
+				$to_add = '$email)';
+
+				$subject = "Clocker - reset hasla" ;
+				$message = "Twoje haslo tymczasowe to: $haslo" ;
+				
+				$headers = "From: $from_add \r\n";
+				$headers .= "Reply-To: $from_add \r\n";
+				$headers .= "Return-Path: $from_add\r\n";
+				$headers .= "X-Mailer: PHP \r\n";
+				
+				
+				if(mail($to_add,$subject,$message,$headers)) 
+				{
+					return true;
+				} 
+				else 
+				{
+					return false;
+				}
+			}
+		}
 	}
 ?>
