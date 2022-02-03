44ba4ae270144202af09843c15e425a5817b1b72
Tescik Tescikowski <74744791+tescik-tescikowski@users.noreply.github.com>
2022-02-03 04:57:30 +0100

    Delete db_.php

--- db_.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DBname', 'clock');
-    define('DBuser', 'clockadmin');
-    define('DBpass', 'VDm9T-Y#8b_Q4qqj');
-    define('DBhost', 'localhost');
-    define('DBport', 3306);
-?>
